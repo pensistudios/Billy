2bb6edce6ff9b8a940e3202e92b97d00e8cef7b4
Faiz Khan <xgrodx@gmail.com>
2012-07-14 23:41:35 -0500

    committing input changes
    input class and playerinput implemented

--- Billy/Billy/PlayerInput.cs
+++ Billy/Billy/PlayerInput.cs
@@ -2,16 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.Diagnostics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 
 namespace Billy
 {
     class PlayerInput : Input
     {
-        public override void Update() 
+        KeyboardState keyboard;
+
+        public new void Update(GameTime gt) 
         {
-        
-            //IMPLEMENT ME!!!
+            // keyboard initialization
+            keyboard = Keyboard.GetState();
+
+            //Debug writes to Debug Console
+           // Debug.Write(goLeft);
+           // Debug.Write(goRight);
+           // Debug.Write(jump);
+           // Debug.Write(drop);
+
+            //Keyboard update code works by checking 
+            //for keys pressed and sets boolean respectively
+            // keyboard uses KeyboardState from XNA 4.0 to gather input
+            if (keyboard.IsKeyDown(Keys.Left) == true) { goLeft = true; }
+                 else { goLeft = false; }
+            if (keyboard.IsKeyDown(Keys.Right) == true) { goRight = true; }
+                 else { goRight = false; }
+            if (keyboard.IsKeyDown(Keys.Up) == true) { jump = true; }
+                 else { jump = false; }
+            if (keyboard.IsKeyDown(Keys.Down) == true) { drop = true; }
+                 else { drop = false; }
+                
         
         }
     }
